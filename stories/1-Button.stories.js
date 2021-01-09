97eca2903bb6d11713a47f91c88d53a25f7fa058
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-09 22:03:29 +0300

    stack and themebutton created

--- stories/1-Button.stories.js
+++ stories/1-Button.stories.js
@@ -3,13 +3,25 @@ import { Home } from '../components/icons'
 import Button from '../components/button'
 import NavigationButton from '../components/navigation-button'
 import Navigation from '../components/navigation'
-import TitleBold from '../components/title-bold'
+import TitleBold from '../components/text-title'
+import ThemeButton from '../components/theme-button'
+import Stack from '../components/stack'
 
 export default {
   title: 'Buttons'
 }
 
 export const Normal = () => <Button>Save</Button>
+
+export const Theme = () => (
+  <Stack column gap={40}>
+    <ThemeButton>Tweet</ThemeButton>
+    <ThemeButton full>Full Tweet</ThemeButton>
+    <ThemeButton full big>
+      Full Big Tweet
+    </ThemeButton>
+  </Stack>
+)
 
 export const NavButton = () => (
   <NavigationButton>

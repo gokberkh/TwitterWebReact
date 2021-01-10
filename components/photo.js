c8496f8c133d9107cb2e7e71efccb6cb054297e5
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-10 22:43:37 +0300

    photo (avatar) created.

---
+++ components/photo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import cn from 'classnames'
+
+import styles from './photo.module.css'
+
+function Photo({
+  src = 'https://pbs.twimg.com/profile_images/1308010958862905345/-SGZioPb_400x400.jpg',
+  alt
+}) {
+  return (
+    <div className={cn([styles.photo])}>
+      <img className={styles.img} src={src} alt={alt} />
+    </div>
+  )
+}
+
+export default Photo

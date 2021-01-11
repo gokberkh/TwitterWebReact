5e8a1e83fffcb6b1ec2f479fd18217c2dd619010
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-11 20:46:55 +0300

    layout created and Homepage fixed.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
-import Button from '../components/button'
+import Layout from '../components/layout'
+import useWindowSize from '../hooks/useWindowSize'
+import CONST from '../constants'
+import Sidebar from '../components/col-sidebar'
+import Extra from '../components/col-extra'
+import Main from '../components/col-main'
 
 function HomePage() {
+  const size = useWindowSize()
+
   return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-      <Button>Merhaba</Button>
-    </div>
+    <Layout>
+      <Sidebar flat={size.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
+      <Main>{JSON.stringify(size)}</Main>
+      {size.width > CONST.TABLET_SIZE && <Extra>extra</Extra>}
+    </Layout>
   )
 }
 

538b6fa6feb078917e088b358d74d699bbfe076a
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-12 22:20:21 +0300

    install @storybook/addon-knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
   stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-knobs'],
   presets: [path.resolve(__dirname, './next-preset.js')]
 }

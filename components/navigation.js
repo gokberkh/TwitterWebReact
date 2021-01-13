164f46792a1f715d114f6c4c326a58bb23b3706c
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-13 20:57:45 +0300

    navigation flat property and iconSelected added

--- components/navigation.js
+++ components/navigation.js
@@ -19,54 +19,63 @@ const MENU = [
   {
     key: 'twitter',
     icon: <Twitter />,
+    iconSelected: <Twitter />,
     title: '',
     notify: 0
   },
   {
     key: 'home',
     icon: <Home />,
+    iconSelected: <HomeFill />,
     title: 'Home',
     notify: 0
   },
   {
     key: 'explore',
     icon: <Explore />,
+    iconSelected: <ExploreFill />,
     title: 'Explore',
     notify: 0
   },
   {
     key: 'notification',
     icon: <Notification />,
+    iconSelected: <NotificationFill />,
     title: 'Notification',
     notify: 17
   },
   {
     key: 'messages',
     icon: <Messages />,
+    iconSelected: <MessagesFill />,
     title: 'Messages',
     notify: 0
   },
   {
     key: 'bookmark',
     icon: <Bookmark />,
+    iconSelected: <BookmarkFill />,
     title: 'Bookmark',
     notify: 0
   },
   {
     key: 'lists',
     icon: <Lists />,
+    iconSelected: <ListsFill />,
     title: 'Lists',
     notify: 0
   },
   {
     key: 'profile',
     icon: <Profile />,
+    iconSelected: <ProfileFill />,
     title: 'Profile',
     notify: 0
   },
   {
     key: 'more',
     icon: <More />,
+    iconSelected: <More />,
     title: 'More',
     notify: 0
   }
@@ -74,17 +83,15 @@ const MENU = [
 
 import styles from './navigation.module.css'
 
-function Navigation({ flat = false, selectedKey }) {
+function Navigation({ flat = false, selectedKey = 'home' }) {
   return (
     <nav className={styles.nav}>
       {MENU.map((menu) => {
         const showTitle = !flat && menu.title.length > 0
+        const selected = selectedKey === menu.key
         return (
-          <NavigationButton
-            notify={menu.notify}
-            selected={selectedKey === menu.key}
-          >
-            {menu.icon}
+          <NavigationButton notify={menu.notify} selected={selected}>
+            {selected ? menu.iconSelected : menu.icon}
             {showTitle && <TitleBold>{menu.title}</TitleBold>}
           </NavigationButton>
         )

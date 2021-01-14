b6c26acf5464be9584f9d0346f320f0134ecb555
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-14 23:06:11 +0300

    homepage fixed.

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,11 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import useWindowSize from '../hooks/useWindowSize'
-import CONST from '../constants'
-import Sidebar from '../components/col-sidebar'
-import Extra from '../components/col-extra'
-import Main from '../components/col-main'
 
 function HomePage() {
   const size = useWindowSize()
 
-  return (
-    <Layout>
-      <Sidebar flat={size.width < CONST.DESKTOP_SIZE}>sidebar</Sidebar>
-      <Main>{JSON.stringify(size)}</Main>
-      {size.width > CONST.TABLET_SIZE && <Extra>extra</Extra>}
-    </Layout>
-  )
+  return <Layout>Anasayfa</Layout>
 }
 
 export default HomePage

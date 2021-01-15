e13c3a818551089bdec88e7ae63e72b2c9eb9368
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-15 22:07:16 +0300

    navigation useRouter added and icon color fixed.

--- components/navigation.js
+++ components/navigation.js
@@ -1,17 +1,20 @@
 import React from 'react'
 
+import { useRouter } from 'next/router'
 import { MENU } from '../constants'
 import styles from './navigation.module.css'
 
 import NavigationButton from './navigation-button'
 import TextTitle from './text-title'
 
-function Navigation({ flat = false, selectedKey = 'home' }) {
+function Navigation({ flat = false }) {
+  const router = useRouter()
+
   return (
     <nav className={styles.nav}>
       {MENU.map((menu) => {
         const showTitle = !flat && menu.title.length > 0
-        const selected = selectedKey === menu.key
+        const selected = router.pathname === menu.path
         return (
           <NavigationButton
             notify={menu.notify}

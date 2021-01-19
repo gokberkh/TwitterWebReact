9abfe3665202799777b785282f3dca2c9ca09369
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-19 21:30:47 +0300

    tweet css fixed and tweet icons added.

--- components/tweet/index.js
+++ components/tweet/index.js
@@ -4,6 +4,8 @@ import Photo from '../photo'
 import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict'
 
 import styles from './style.module.css'
+import IconButton from '../icon'
+import * as Icon from '../icons'
 
 function Tweet({ name, slug, datetime, text }) {
   return (
@@ -15,10 +17,41 @@ function Tweet({ name, slug, datetime, text }) {
       {/*avatar*/}
       <div className={styles.body}>
         <header className={styles.header}>
-          {name} @{slug} ·{formatDistanceToNowStrict(datetime)}
+          <span className={styles.name}>{name} </span> <span>@{slug}</span> ·{' '}
+          <span>{formatDistanceToNowStrict(datetime)}</span>
         </header>
         <div className={styles.content}>{text}</div>
-        <footer className={styles.footer}>footer</footer>
+        <footer className={styles.footer}>
+          {/* reply */}
+          <div className={styles.footerButton}>
+            <IconButton className={styles.actionButton}>
+              <Icon.Reply></Icon.Reply>
+            </IconButton>
+            <span>5</span>
+          </div>
+
+          {/* retweet */}
+          <div className={styles.footerButton}>
+            <IconButton className={styles.actionButton}>
+              <Icon.Retweet />
+            </IconButton>
+            <span>58</span>
+          </div>
+
+          {/* like */}
+          <div className={styles.footerButton}>
+            <IconButton className={styles.actionButton}>
+              <Icon.Like></Icon.Like>
+            </IconButton>
+          </div>
+
+          {/* share */}
+          <div className={styles.footerButton}>
+            <IconButton className={styles.actionButton}>
+              <Icon.Share></Icon.Share>
+            </IconButton>
+          </div>
+        </footer>
       </div>
     </article>
   )

f6368daa0502a9999fb123de61149536f05d0088
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-21 22:43:19 +0300

    fake data (because there is not twitter developer account)

--- pages/api/tweet.js
+++ pages/api/tweet.js
@@ -2,14 +2,20 @@ import Twit from '../../lib/twit'
 
 const data = [
   {
+    favorite_count: 1,
+    retweet_count: 11,
+    favorited: true,
+    retweet: true,
     created_at: 'Thu Apr 06 15:24:15 +0000 2017',
     id_str: '850006245121695744',
     text:
-      '1/ Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://t.co/XweGngmxlP',
+      'Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
     user: {
       id: 2244994945,
       name: 'Twitter Dev',
       screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
       location: 'Internet',
       url: 'https://dev.twitter.com/',
       description:
@@ -20,7 +26,7 @@ const data = [
       hashtags: [],
       urls: [
         {
-          url: 'https://t.co/XweGngmxlP',
+          url: 'https://developer.twitter.com/en',
           unwound: {
             url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
             title: 'Building the Future of the Twitter API Platform'
@@ -31,14 +37,20 @@ const data = [
     }
   },
   {
+    favorite_count: 1,
+    retweet_count: 11,
+    favorited: true,
+    retweet: true,
     created_at: 'Thu Apr 06 15:24:15 +0000 2017',
     id_str: '850006245121695744',
     text:
-      '1/ Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://t.co/XweGngmxlP',
+      'Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
     user: {
       id: 2244994945,
       name: 'Twitter Dev',
       screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
       location: 'Internet',
       url: 'https://dev.twitter.com/',
       description:
@@ -49,7 +61,253 @@ const data = [
       hashtags: [],
       urls: [
         {
-          url: 'https://t.co/XweGngmxlP',
+          url: 'https://developer.twitter.com/en',
+          unwound: {
+            url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
+            title: 'Building the Future of the Twitter API Platform'
+          }
+        }
+      ],
+      user_mentions: []
+    }
+  },
+  {
+    favorite_count: 1,
+    retweet_count: 11,
+    favorited: true,
+    retweet: true,
+    created_at: 'Thu Apr 06 15:24:15 +0000 2017',
+    id_str: '850006245121695744',
+    text:
+      'Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
+    user: {
+      id: 2244994945,
+      name: 'Twitter Dev',
+      screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
+      location: 'Internet',
+      url: 'https://dev.twitter.com/',
+      description:
+        'Your official source for Twitter Platform news, updates & events. Need technical help? Visit https://twittercommunity.com/ \u2328\ufe0f #TapIntoTwitter'
+    },
+    place: {},
+    entities: {
+      hashtags: [],
+      urls: [
+        {
+          url: 'https://developer.twitter.com/en',
+          unwound: {
+            url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
+            title: 'Building the Future of the Twitter API Platform'
+          }
+        }
+      ],
+      user_mentions: []
+    }
+  },
+  {
+    favorite_count: 1,
+    retweet_count: 11,
+    favorited: true,
+    retweet: true,
+    created_at: 'Thu Apr 06 15:24:15 +0000 2017',
+    id_str: '850006245121695744',
+    text:
+      'Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
+    user: {
+      id: 2244994945,
+      name: 'Twitter Dev',
+      screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
+      location: 'Internet',
+      url: 'https://dev.twitter.com/',
+      description:
+        'Your official source for Twitter Platform news, updates & events. Need technical help? Visit https://twittercommunity.com/ \u2328\ufe0f #TapIntoTwitter'
+    },
+    place: {},
+    entities: {
+      hashtags: [],
+      urls: [
+        {
+          url: 'https://developer.twitter.com/en',
+          unwound: {
+            url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
+            title: 'Building the Future of the Twitter API Platform'
+          }
+        }
+      ],
+      user_mentions: []
+    }
+  },
+  {
+    favorite_count: 1,
+    retweet_count: 11,
+    favorited: true,
+    retweet: true,
+    created_at: 'Thu Apr 06 15:24:15 +0000 2017',
+    id_str: '850006245121695744',
+    text:
+      'Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
+    user: {
+      id: 2244994945,
+      name: 'Twitter Dev',
+      screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
+      location: 'Internet',
+      url: 'https://dev.twitter.com/',
+      description:
+        'Your official source for Twitter Platform news, updates & events. Need technical help? Visit https://twittercommunity.com/ \u2328\ufe0f #TapIntoTwitter'
+    },
+    place: {},
+    entities: {
+      hashtags: [],
+      urls: [
+        {
+          url: 'https://developer.twitter.com/en',
+          unwound: {
+            url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
+            title: 'Building the Future of the Twitter API Platform'
+          }
+        }
+      ],
+      user_mentions: []
+    }
+  },
+  {
+    favorite_count: 1,
+    retweet_count: 11,
+    favorited: true,
+    retweet: true,
+    created_at: 'Thu Apr 06 15:24:15 +0000 2017',
+    id_str: '850006245121695744',
+    text:
+      'Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
+    user: {
+      id: 2244994945,
+      name: 'Twitter Dev',
+      screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
+      location: 'Internet',
+      url: 'https://dev.twitter.com/',
+      description:
+        'Your official source for Twitter Platform news, updates & events. Need technical help? Visit https://twittercommunity.com/ \u2328\ufe0f #TapIntoTwitter'
+    },
+    place: {},
+    entities: {
+      hashtags: [],
+      urls: [
+        {
+          url: 'https://developer.twitter.com/en',
+          unwound: {
+            url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
+            title: 'Building the Future of the Twitter API Platform'
+          }
+        }
+      ],
+      user_mentions: []
+    }
+  },
+  {
+    favorite_count: 1,
+    retweet_count: 11,
+    favorited: true,
+    retweet: true,
+    created_at: 'Thu Apr 06 15:24:15 +0000 2017',
+    id_str: '850006245121695744',
+    text:
+      'Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
+    user: {
+      id: 2244994945,
+      name: 'Twitter Dev',
+      screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
+      location: 'Internet',
+      url: 'https://dev.twitter.com/',
+      description:
+        'Your official source for Twitter Platform news, updates & events. Need technical help? Visit https://twittercommunity.com/ \u2328\ufe0f #TapIntoTwitter'
+    },
+    place: {},
+    entities: {
+      hashtags: [],
+      urls: [
+        {
+          url: 'https://developer.twitter.com/en',
+          unwound: {
+            url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
+            title: 'Building the Future of the Twitter API Platform'
+          }
+        }
+      ],
+      user_mentions: []
+    }
+  },
+  {
+    favorite_count: 1,
+    retweet_count: 11,
+    favorited: true,
+    retweet: true,
+    created_at: 'Thu Apr 06 15:24:15 +0000 2017',
+    id_str: '850006245121695744',
+    text:
+      'Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
+    user: {
+      id: 2244994945,
+      name: 'Twitter Dev',
+      screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
+      location: 'Internet',
+      url: 'https://dev.twitter.com/',
+      description:
+        'Your official source for Twitter Platform news, updates & events. Need technical help? Visit https://twittercommunity.com/ \u2328\ufe0f #TapIntoTwitter'
+    },
+    place: {},
+    entities: {
+      hashtags: [],
+      urls: [
+        {
+          url: 'https://developer.twitter.com/en',
+          unwound: {
+            url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
+            title: 'Building the Future of the Twitter API Platform'
+          }
+        }
+      ],
+      user_mentions: []
+    }
+  },
+
+  {
+    favorite_count: 58,
+    retweet_count: 55,
+    favorited: true,
+    retweet: true,
+    created_at: 'Thu Apr 06 15:24:15 +0000 2017',
+    id_str: '850006245121695744',
+    text:
+      ' Today we\u2019re sharing our vision for the future of the Twitter API platform!\nhttps://developer.twitter.com/en',
+    user: {
+      id: 2244994945,
+      name: 'Twitter Dev',
+      screen_name: 'TwitterDev',
+      profile_image_url_https:
+        'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
+      location: 'Internet',
+      url: 'https://dev.twitter.com/',
+      description:
+        'Your official source for Twitter Platform news, updates & events. Need technical help? Visit https://twittercommunity.com/ \u2328\ufe0f #TapIntoTwitter'
+    },
+    place: {},
+    entities: {
+      hashtags: [],
+      urls: [
+        {
+          url: 'https://developer.twitter.com/en',
           unwound: {
             url: 'https://cards.twitter.com/cards/18ce53wgo4h/3xo1c',
             title: 'Building the Future of the Twitter API Platform'

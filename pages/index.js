fc0558980931d47fb9160d7d6bee6a308f399125
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-21 22:35:34 +0300

    swr install and fetcher

--- pages/index.js
+++ pages/index.js
@@ -3,30 +3,34 @@ import React from 'react'
 import Layout from '../components/layout'
 import Tweet from '../components/tweet'
 import useWindowSize from '../hooks/useWindowSize'
+import fetcher from '../lib/fetch'
+import useSWR from 'swr'
 
 function HomePage() {
+  const { data } = useSWR('/api/tweet', fetcher)
+
+  /* const [data, dataSet] = React.useState([])
+  const [dataLoading, dataLoadingSet] = React.useState(true)*/
+
   const size = useWindowSize()
 
+  /* React.useEffect(() => {
+    async function getData() {
+      const response = await fetch('http://localhost:3000/api/tweet')
+      const data = await response.json()
+      dataSet(data)
+      dataLoadingSet(false)
+      console.log(data)
+    }
+    getData()
+  }, [])*/
   return (
     <Layout>
-      <Tweet
-        name="Gökberk H."
-        slug="gokberkh"
-        datetime={new Date('2021-01-01')}
-        text={`Merhaba ilk tweet  :))
-        
+      {!data && <p>Loading</p>}
 
-NEXT , REACT , STORYBOOK ...`}
-      ></Tweet>
-      <Tweet
-        name="Gökberk H."
-        slug="gokberkh"
-        datetime={new Date('2021-01-01')}
-        text={`Merhaba ikinci tweet  :)
-        
-
-NEXT , REACT , STORYBOOK ...`}
-      ></Tweet>
+      {data?.map((tweet) => {
+        return <Tweet key={tweet.id} {...tweet} />
+      })}
     </Layout>
   )
 }

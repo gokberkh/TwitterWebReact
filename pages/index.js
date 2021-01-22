55d4bc2a013b4809a634cce0851399afc5cd08a7
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-22 23:08:30 +0300

    homepage fixed.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Layout from '../components/layout'
 import Tweet from '../components/tweet'
-import useWindowSize from '../hooks/useWindowSize'
+
 import fetcher from '../lib/fetch'
 import useSWR from 'swr'
 import Loading from '../components/loading'
@@ -11,22 +11,6 @@ import styles from './index.module.css'
 function HomePage() {
   const { data } = useSWR('/api/tweet', fetcher)
 
-  /* const [data, dataSet] = React.useState([])
-  const [dataLoading, dataLoadingSet] = React.useState(true)*/
-
-  //const size = useWindowSize()
-
-  //FETCH
-  /* React.useEffect(() => {
-    async function getData() {
-      const response = await fetch('http://localhost:3000/api/tweet')
-      const data = await response.json()
-      dataSet(data)
-      dataLoadingSet(false)
-      console.log(data)
-    }
-    getData()
-  }, [])*/
   return (
     <Layout>
       {!data && (

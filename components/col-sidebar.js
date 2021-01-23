d61914dee6ad5472e71125a1a29d629749eef8d9
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-23 18:55:03 +0300

    tweet-modal added to sidebar

--- components/col-sidebar.js
+++ components/col-sidebar.js
@@ -4,16 +4,21 @@ import Navigation from './navigation'
 import ThemeButton from './theme-button'
 import ProfileBox from './profile-box'
 import styles from './col-sidebar.module.css'
+import TweetModal from './tweet-modal'
 
 function Layout({ flat }) {
   return (
     <div className={cn([styles.sidebar])}>
       <Navigation flat={flat} />
+
       <div className={styles.tweet}>
         <ThemeButton big full={!flat}>
           {flat ? 'a' : 'Tweet'}
         </ThemeButton>
       </div>
+
+      {/*tweet popup*/}
+      <TweetModal></TweetModal>
       <div className={styles.profile}>
         <ProfileBox flat={flat} />
       </div>

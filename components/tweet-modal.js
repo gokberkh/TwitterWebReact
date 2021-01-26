e55d94f6c5d6164ec08915822aba1dc717435d29
Gökberk Hıdıroğlu <gokbey5801@gmail.com>
2021-01-26 22:16:40 +0300

    fake twitter post api for jsonserver

--- components/tweet-modal.js
+++ components/tweet-modal.js
@@ -13,17 +13,40 @@ function TweetModal({ onModalClose = () => {}, onClick = () => {} }) {
 
   const onSubmit = async () => {
     try {
-      const response = await fetch('/api/new', {
+      //json-server --watch db.json --port 3004
+      //post method
+
+      const response = await fetch(' http://localhost:3004/data', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ tweet })
+        body: JSON.stringify({
+          favorite_count: 1,
+          retweet_count: 11,
+          favorited: true,
+          retweet: true,
+          created_at: 'Thu Jan 21 22:00:00 +0000 2021',
+          id_str: '850006245121695744',
+          text: tweet,
+          user: {
+            id: 2244994945,
+            name: 'Gokberk H',
+            screen_name: 'gokberkh',
+            profile_image_url_https:
+              'https://avatars0.githubusercontent.com/u/25930033?s=400&u=b4daefc78faea6f9d8a6bfdb7bdc1ed566453fd5&v=4',
+            location: 'Internet',
+            url: 'https://dev.twitter.com/',
+            description:
+              'Your official source for Twitter Platform news, updates & events. Need technical help? Visit https://twittercommunity.com/ \u2328\ufe0f #TapIntoTwitter'
+          }
+        })
       })
-      if (response.status !== 200) throw 'Twet gönderilemedi.'
 
       tweetSet('')
       onModalClose()
+
+      if (response.status !== 201) throw 'Twet gönderilemedi.'
     } catch (e) {
       console.log(e)
     }
